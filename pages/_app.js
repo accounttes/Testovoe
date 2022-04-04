c98ad27ba2a7936e98e6b4b6bf4781639839fdf7
imyapolza <imyapolza7@gmail.com>
2022-04-05 02:11:16 +0500

    ts useWatch react-bootsrtap buttons

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.scss';
-import { wrapper } from '../store';
-
-// This default export is required in a new `pages/_app.js` file.
-const WrappedApp = function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-};
-
-export default wrapper.withRedux(WrappedApp);
